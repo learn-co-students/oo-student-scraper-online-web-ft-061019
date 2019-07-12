9c1b83367032e4fc76b0f72f0e3c0000ba3b0b7a
Alex Nahimana <alex.nahimana1@gmail.com>
2019-07-12 08:43:41 -0500

    Fix .scrape_profile_page method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,6 +20,29 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
+    doc = Nokogiri::HTML(open(profile_url))
+
+    my_hash = {}
+    social_links = doc.css('.social-icon-container a').reject do |container|
+      container.attributes["href"].value.nil?
+    end
+
+    social_links.each do |link|
+      social_sym = [:twitter, :linkedin, :github]
+      url = link.attributes["href"].value
+
+      if URI.parse(url).host.split(".").include?("www")
+         my_sym = URI.parse(url).host.split(".")[1].to_sym
+      else
+        my_sym = URI.parse(url).host.split(".")[0].to_sym
+      end
+      social_sym.include?(my_sym)? (my_hash[my_sym] = url) : (my_hash[:blog] = url)
+    end
+
+    my_hash[:profile_quote] = doc.css('div.profile-quote').text
+    my_hash[:bio] = doc.css('.description-holder p').text
+
+    my_hash
 
   end
 
