7e8632c255fc54961fbc69f8af4247b02e7f80f8
Alex Nahimana <alex.nahimana1@gmail.com>
2019-07-12 08:42:45 -0500

    Fix .scrape_index_page method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,11 +4,23 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+
+    [].tap do |arr|
+      doc.css(".student-card").each do |card|
+        arr << {
+          :name => card.css(".student-name").first.text,
+          :location => card.css(".student-location").first.text,
+          :profile_url =>  card.css("a").first.attributes["href"].value,
+        }
+
+      end
+    end
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
